4bbc6b6d599e0f7319e13ca705645b2e319c9215
Komiyama-Tsubasa <arwins0315@docomo.ne.jp>
2017-05-25 09:00:23 +0900

    Add files via upload

---
+++ Four arithmetic operations.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import sys
+while 1:
+    formula = eval(input("式を入力してください:"))
+    if formula == exit:
+        break
+    else :
+        print("答えは" + str(formula) + " です。")
+print("終了します")
+sys.exit()
